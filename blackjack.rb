446eef83aff9ae61ddde6dcf716611cf233883da
Michael E. Nash <utumno86@gmail.com>
2015-05-07 21:46:15 -0400

    Futzed with the value total counters to rationalize the code

--- blackjack.rb
+++ blackjack.rb
@@ -10,12 +10,16 @@ require 'deck'
 @dealer_hand =[]
 @player_done = false
 @dealer_done = false
+@player_value_total = 0
+@dealer_value_total = 0
 
 def deal (id)
   if id == 1
     @player_hand << @new_deck.cards[@card_index]
+    @player_value_total += @new_deck.cards[@card_index].value
   elsif id == 2
     @dealer_hand << @new_deck.cards[@card_index]
+    @dealer_value_total += @new_deck.cards[@card_index].value
   else
     puts "Somehow you broke the program and dealt cards to a third party. Game goes to Cthulu."
   end
@@ -30,21 +34,17 @@ deal(1)
 deal(2)
 
 unless @player_done && @dealer_done == true
-  player_value_total = 0
-  dealer_value_total = 0
   
   puts "Your hand:"
-  
   @player_hand.each do |x|
     puts x.print_name
-    player_value_total += x.value
-    puts player_value_total
+    puts @player_value_total
   end
 
+  puts "Dealer hand:"
   @dealer_hand.each do |x|
     puts x.print_name
-    dealer_value_total += x.value
-    puts dealer_value_total
+    puts @dealer_value_total
   end
 
   @player_done = true
